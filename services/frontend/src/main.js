4b83a2722293e4a1283b26be622ca2dba3951b75
Gabriel Rojas <gabo06123@gmail.com>
2022-07-06 22:49:36 -0400

    feat: frontend 'helloWorld'

--- services/frontend/src/main.js
+++ services/frontend/src/main.js
@@ -1,5 +1,10 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import "bootstrap/dist/css/bootstrap.css";
+import axios from "axios";
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
 
-createApp(App).use(router).mount('#app')
+axios.defaults.withCredentials = true;
+axios.defaults.baseURL = "http://localhost:5000/";
+
+createApp(App).use(router).mount("#app");
