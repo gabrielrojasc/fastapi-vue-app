8521593b72296fc1c765d3527624b8b8b588f617
Gabriel Rojas <gabo06123@gmail.com>
2022-07-07 00:29:11 -0400

    feat: created routing for the api

--- services/backend/src/main.py
+++ services/backend/src/main.py
@@ -7,6 +7,8 @@ from src.database.config import TORTOISE_ORM
 
 Tortoise.init_models(["src.database.models"], "models")
 
+from src.routes import users, notes
+
 app = FastAPI()
 
 app.add_middleware(
@@ -16,6 +18,8 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+app.include_router(users.router)
+app.include_router(notes.router)
 
 register_tortoise(app, config=TORTOISE_ORM, generate_schemas=False)
 
